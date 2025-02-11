0ee1ecade945a577f505b58999fb0ea06e62de78
Jessy Avalos Barrales <avalosbarralesjessy@gmail.com>
2025-02-11 14:42:05 -0600

    code-fixes

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,7 @@
 import cors from 'cors';
 import dotenv from 'dotenv';
 import morgan from 'morgan';
-import express, { Request, Response, NextFunction } from 'express';
+import express from 'express';
 
 dotenv.config();
 
