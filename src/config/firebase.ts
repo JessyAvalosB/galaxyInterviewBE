80de47f7a9e92fb7c8569ccdcad59194020469b1
Jessy Avalos Barrales <avalosbarralesjessy@gmail.com>
2025-02-11 14:42:23 -0600

    firebase config protected

--- src/config/firebase.ts
+++ src/config/firebase.ts
@@ -1,19 +1,9 @@
 import dotenv from 'dotenv';
 import { initializeApp } from 'firebase/app';
 import { getFirestore } from 'firebase/firestore';
+import firebaseConfig from './firebaseConfig.json'
 
 dotenv.config();
-
-const firebaseConfig = {
-    apiKey: "AIzaSyA0CpCPCcJcwxE2bz6ulF3DLmVGrQkyy2c",
-    authDomain: "galaxy-interview.firebaseapp.com",
-    databaseURL: "https://galaxy-interview-default-rtdb.firebaseio.com",
-    projectId: "galaxy-interview",
-    storageBucket: "galaxy-interview.firebasestorage.app",
-    messagingSenderId: "612683979648",
-    appId: "1:612683979648:web:5dfc23b087ae8ffa793f65",
-    measurementId: "G-MCJN096R05"
-};
 
 const app = initializeApp(firebaseConfig);
 
